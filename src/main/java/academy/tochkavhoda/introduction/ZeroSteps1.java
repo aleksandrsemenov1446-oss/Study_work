3c94d4d4760edfd6c4433eff9753a2a590368035
Semenov_Al <aleksandrsemenov1446@gmail.com>
2026-01-20 22:43:09 +0600

    простейшие вычисления

---
+++ src/main/java/academy/tochkavhoda/introduction/ZeroSteps1.java
@@ -0,0 +1,12 @@
+package academy.tochkavhoda.introduction;
+
+public class ZeroSteps1 {
+
+    public int sum (int x, int y){
+        return x + y;
+    }
+
+    public int mul (int x, int y){
+        return x * y;
+    }
+}
